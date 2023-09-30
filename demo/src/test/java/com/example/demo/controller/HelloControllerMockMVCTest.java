1e32995172fd29eb648776f7cd159bcaa7a2ad3b
anushkadeshpande <anushkaa5000@gmail.com>
2023-09-30 20:44:10 +0530

    test: added integration tests

---
+++ demo/src/test/java/com/example/demo/controller/HelloControllerMockMVCTest.java
@@ -0,0 +1,34 @@
+package com.example.demo.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(HelloController.class)
+public class HelloControllerMockMVCTest {
+    
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void testHelloWithoutName() throws Exception {
+        mvc.perform(get("/hello").accept(MediaType.TEXT_HTML))
+           .andExpect(status().isOk())
+           .andExpect(view().name("hello"))
+           .andExpect(model().attribute("user", "world"));
+
+    }
+
+    @Test
+    public void testHelloWithName() throws Exception {
+        mvc.perform(get("/hello").param("name", "Anna").accept(MediaType.TEXT_HTML))
+           .andExpect(status().isOk())
+           .andExpect(view().name("hello"))
+           .andExpect(model().attribute("user", "Anna"));
+    }
+}
