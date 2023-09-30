b1bc3d125f9bfbbedd9380330566a58d59513a52
anushkadeshpande <anushkaa5000@gmail.com>
2023-10-01 02:22:06 +0530

    test: added a test to check the beans created

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Autowired
+	private ApplicationContext context;
+
 	@Test
 	void contextLoads() {
+		System.out.println("No of beans: " + context.getBeanDefinitionCount());
+
+		for(String name: context.getBeanDefinitionNames())
+			System.out.println(name);
 	}
 
 }
