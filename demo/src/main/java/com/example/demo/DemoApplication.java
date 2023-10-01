8680400eba09b7ce22d57336ba1ae6f75cc4a7cb
anushkadeshpande <anushkaa5000@gmail.com>
2023-10-01 18:58:59 +0530

    feat: added greeting bean

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,20 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.demo.json.Greeting;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public Greeting defaultGreeting() {
+		return new Greeting("Hiee!");
 	}
 
 }
