152ebe0036d0ae7f3dadd9df3e8042af015f6517
anushkadeshpande <anushkaa5000@gmail.com>
2023-10-08 18:42:00 +0530

    test: Added test to check if a particular record exists

--- persistence/src/test/java/com/example/persistence/dao/OfficerRepositoryTest.java
+++ persistence/src/test/java/com/example/persistence/dao/OfficerRepositoryTest.java
@@ -1,10 +1,15 @@
 package com.example.persistence.dao;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.persistence.entities.Officer;
 
 @SpringBootTest
 public class OfficerRepositoryTest {
@@ -16,5 +21,12 @@ public class OfficerRepositoryTest {
         var officers = repository.findAll();
         System.out.println(officers);
         assertEquals(5,officers.size());
+    }
+
+    @Test
+    void findById() {
+        Optional<Officer> officer = repository.findById(1);
+        System.out.println(officer);
+        assertTrue(officer.isPresent());
     }
 }
