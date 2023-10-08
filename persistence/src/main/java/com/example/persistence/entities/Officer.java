f5130bd4434c13c294c3c81d939f5133302ba2c4
anushkadeshpande <anushkaa5000@gmail.com>
2023-10-08 18:19:43 +0530

    feat: Added Officer and Rank entities

---
+++ persistence/src/main/java/com/example/persistence/entities/Officer.java
@@ -0,0 +1,103 @@
+package com.example.persistence.entities;
+
+public class Officer {
+    private Integer id;
+    private Rank rank;
+    private String firstName;
+    private String lastName;
+
+    // default constructor is required for jpa, not for jdbc
+    public Officer() {}
+
+    public Officer(Rank rank, String firstName, String lastName) {
+        this.rank = rank;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Officer(Integer id, Rank rank, String firstName, String lastName) {
+        this.id = id;
+        this.rank = rank;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
+
+    public void setRank(Rank rank) {
+        this.rank = rank;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Officer [id=" + id + ", rank=" + rank + ", firstName=" + firstName + ", lastName=" + lastName + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((rank == null) ? 0 : rank.hashCode());
+        result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+        result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Officer other = (Officer) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (rank != other.rank)
+            return false;
+        if (firstName == null) {
+            if (other.firstName != null)
+                return false;
+        } else if (!firstName.equals(other.firstName))
+            return false;
+        if (lastName == null) {
+            if (other.lastName != null)
+                return false;
+        } else if (!lastName.equals(other.lastName))
+            return false;
+        return true;
+    }
+
+    
+}
