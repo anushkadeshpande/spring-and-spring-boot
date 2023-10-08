ab409eb1a32f9091f024f1fb2971a202e54cef28
anushkadeshpande <anushkaa5000@gmail.com>
2023-10-08 16:44:18 +0530

    refactor: Changed sysout statements to log statements

--- restclient/src/test/java/com/example/restclient/services/AstroServiceTest.java
+++ restclient/src/test/java/com/example/restclient/services/AstroServiceTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
@@ -15,27 +17,29 @@ import com.example.restclient.json.AstroResponse;
 @SpringBootTest
 public class AstroServiceTest {
 
+    private final Logger log = LoggerFactory.getLogger(AstroServiceTest.class);
+
     @Autowired
     private AstroService service;
 
     @Test
     void testGetAstronauts() {
         String json = service.getAstronauts();
-        System.out.println(json);
+        log.info(json);
         assertTrue(json.contains("success"));
     }
 
     @Test
     void testGetAstroResponse() {
         var response = service.getAstroResponse();
-        System.out.println(response);
+        log.info(response.toString());
         assertEquals("success", response.getMessage());        
     }
 
     @Test
     void testGetAstroResponseEntity() {
         ResponseEntity<AstroResponse> response = service.getAstroResponseEntity();
-        System.out.println(response);
+        log.info(response.toString());
         assertNotNull(response.getBody());
         assertEquals(HttpStatus.OK, response.getStatusCode());
         assertEquals("success", response.getBody().getMessage());
@@ -44,7 +48,7 @@ public class AstroServiceTest {
     @Test
     void testGetAstroResponseRestTemplate() {
         var response = service.getAstroResponse();
-        System.out.println(response);
+        log.info(response.toString());
         assertEquals("success", response.getMessage()); 
     }
 
