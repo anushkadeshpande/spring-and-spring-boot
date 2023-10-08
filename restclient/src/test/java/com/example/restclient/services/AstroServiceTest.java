6dadeb6739ad90d361d789f2bdc5e37aaf12b4ae
anushkadeshpande <anushkaa5000@gmail.com>
2023-10-08 16:34:03 +0530

    test: Added test for the returned ResponseEntity

--- restclient/src/test/java/com/example/restclient/services/AstroServiceTest.java
+++ restclient/src/test/java/com/example/restclient/services/AstroServiceTest.java
@@ -1,11 +1,16 @@
 package com.example.restclient.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.example.restclient.json.AstroResponse;
 
 @SpringBootTest
 public class AstroServiceTest {
@@ -25,6 +30,15 @@ public class AstroServiceTest {
         var response = service.getAstroResponse();
         System.out.println(response);
         assertEquals("success", response.getMessage());        
+    }
+
+    @Test
+    void testGetAstroResponseEntity() {
+        ResponseEntity<AstroResponse> response = service.getAstroResponseEntity();
+        System.out.println(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals("success", response.getBody().getMessage());
     }
 
     
