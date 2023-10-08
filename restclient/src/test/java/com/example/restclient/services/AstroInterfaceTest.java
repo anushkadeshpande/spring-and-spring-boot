702629a5df67de362cc593648fbfa511d592832c
anushkadeshpande <anushkaa5000@gmail.com>
2023-10-08 16:57:00 +0530

    test: Added test for HTTP Interface call

---
+++ restclient/src/test/java/com/example/restclient/services/AstroInterfaceTest.java
@@ -0,0 +1,27 @@
+package com.example.restclient.services;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.restclient.json.AstroResponse;
+
+@SpringBootTest
+public class AstroInterfaceTest {
+
+    private final Logger log = LoggerFactory.getLogger(AstroInterfaceTest.class);
+
+    @Autowired
+    private AstroInterface service;
+
+    @Test
+    void testGetAstroResponse() {
+        AstroResponse response = service.getAstroResponse();
+        log.info(response.toString());
+        assertEquals("success", response.getMessage());
+    }
+}
