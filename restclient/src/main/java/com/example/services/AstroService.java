295c485094223095aaabab17eab9f73227c9b923
anushkadeshpande <anushkaa5000@gmail.com>
2023-10-08 14:58:02 +0530

    feat: Added RestTemplate in AstroService class

--- restclient/src/main/java/com/example/services/AstroService.java
+++ restclient/src/main/java/com/example/services/AstroService.java
@@ -1,6 +1,18 @@
 package com.example.services;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+
 @Service
 public class AstroService {
-    
+
+    // webclient is not a managed bean
+    private final WebClient client = WebClient.create("http://api.open-notify.org");
+    private final RestTemplate template;
+
+    public AstroService(RestTemplateBuilder builder)  {
+        template = builder.build();
+    }
 }
