159477f3d96d262958c80610aaa418fbfaf1a154
anushkadeshpande <anushkaa5000@gmail.com>
2023-10-29 01:01:46 +0530

    test: Added test to print the port on which the app is running

--- shopping/src/test/java/com/example/shopping/controllers/ProductControllerFunctionalTest.java
+++ shopping/src/test/java/com/example/shopping/controllers/ProductControllerFunctionalTest.java
@@ -1,10 +1,13 @@
 package com.example.shopping.controllers;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
@@ -19,8 +22,17 @@ public class ProductControllerFunctionalTest {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    @LocalServerPort
+    int port;
+
     private List<Integer> getIds() {
         return jdbcTemplate.queryForList("SELECT id FROM product", Integer.class);
+    }
+
+    @Test
+    void printPort() {
+        assertNotNull(port);
+        System.out.println("APPLICATION IS RUNNING ON PORT: "+ port);
     }
 
     @Test
